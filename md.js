129bc2d7ed5d695a2fac34c7b037c03114d54ff2
Mehul Tikekar <mehultikekar@gmail.com>
2015-06-22 03:18:24 -0400

    catch katex error

--- md.js
+++ md.js
@@ -1,12 +1,20 @@
 var katex = require('katex');
 
+function latexRender(str) {
+    try {
+        return katex.renderToString(str);
+    } catch (err) {
+        return str;
+    }
+}
+
 var md = require('markdown-it')({
     html: true,
     linkify: true,
     typographer: true
 })
 .use(require('markdown-it-emoji'))
-.use(require('markdown-it-simplemath'), {inlineRenderer: katex.renderToString})
+.use(require('markdown-it-simplemath'), {inlineRenderer: latexRender})
 .use(require('markdown-it-sanitizer'));
 
 var twemoji = require('twemoji');
